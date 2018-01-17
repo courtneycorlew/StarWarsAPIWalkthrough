657598ebba00d243bbca1d1d43044f89d103aca9
CourtneyCorlew <courtneyecorlew@gmail.com>
2018-01-17 16:58:41 -0500

    added ajax request

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 // alert('working!')
 // })
 
+
+
+$(() => {
+  $('#infoButton').click(() => {
+    $.ajax({
+      type: 'GET',
+      url: 'https://swapi.co/api/people'
+    }).done((res) => {
+      let people = res.results
+      for (person of people) {
+        console.log(person.name)
+      }
+
+    })
+  })
+})
+
