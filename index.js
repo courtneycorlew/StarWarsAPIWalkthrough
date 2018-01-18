897d40e9041b7ad06bdf23c0c6940ad059439cd0
CourtneyCorlew <courtneyecorlew@gmail.com>
2018-01-18 10:40:33 -0500

    added clear button and ajax requests

--- index.js
+++ index.js
@@ -6,16 +6,46 @@
 
 
 $(() => {
+
+  let loaded = false;
+
   $('#infoButton').click(() => {
+    if(!loaded) {
     $.ajax({
       type: 'GET',
       url: 'https://swapi.co/api/people'
     }).done((res) => {
       let people = res.results
-      for (person of people) {
-        console.log(person.name)
+      for (p of people) {
+      $('#tableBody').append(createTableRow(p))
       }
+      loaded = true;
     })
+  }
   })
+
+
+
+  function createTableRow(person) {
+    let row = $(`<tr></tr>`) 
+    let name = $(`<td> ${person.name}</td>`)
+    let height = $(`<td>${person.height}</td>`)
+    let birth = $(`<td> ${person.birth_year}</td>`)
+
+    row.append(name)
+    row.append(height)
+    row.append(birth)
+    
+    return row
+  }
+
+
+
+
+
+
+
+
+
 })
 
